190e0b32c823e85edbe2dd23d52dea2eec4bb283
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-02-12 11:25:04 -0800

    used useReduser for change state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action === "add") return { money: state.money + 10 };
+  if (action === "take") return { money: state.money - 10 };
+  return new Error();
+};
 
 function App() {
+  const initialState = { money: 100 };
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Wallet : {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({ type: "add" })}>
+          {" "}
+          Add money to wallet
+        </button>
+        <button onClick={() => dispatch({ type: "take" })}>
+          {" "}
+          Take money from wallet
+        </button>
+      </div>
     </div>
   );
 }
