3873ed8a739b83130e2a14fb8990cf8de94d6bd1
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2024-01-12 09:08:30 -0800

    test commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useReducer } from "react";
+// import DatePicker from "./components/DatePicker";
 
 const reducer = (state, action) => {
   if (action.type === "add") return { money: state.money + 10 };
@@ -7,24 +8,30 @@ const reducer = (state, action) => {
   return new Error();
 };
 
+//The dispatch function returned by useReducer lets you update the state to a different value and trigger a re-render. You need to pass the action as the only argument to the dispatch function:
+
 function App() {
   const initialState = { money: 100 };
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className="App">
-      <h1> Wallet : {state.money}</h1>
-      <div>
-        <button onClick={() => dispatch({ type: "add" })}>
-          {" "}
-          Add money to wallet
-        </button>
-        <button onClick={() => dispatch({ type: "take" })}>
-          {" "}
-          Take money from wallet
-        </button>
+    <>
+      {" "}
+      <div className="App">
+        <h1> Wallet : {state.money}</h1>
+        <div>
+          <button onClick={() => dispatch({ type: "add" })}>
+            {" "}
+            Add money to wallet
+          </button>
+          <button onClick={() => dispatch({ type: "take" })}>
+            {" "}
+            Take money from wallet
+          </button>
+        </div>
       </div>
-    </div>
+      {/* <DatePicker /> */}
+    </>
   );
 }
 
